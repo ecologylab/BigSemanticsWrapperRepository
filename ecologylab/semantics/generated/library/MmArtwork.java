e9b3c826afd54000bd86b08d30cc35504c138654
nlupfer <nlupfer@ecologylab.net>
2010-02-05 20:08:25 +0000

    fixes for mmArtwork

--- ecologylab/semantics/generated/library/MmArtwork.java
+++ ecologylab/semantics/generated/library/MmArtwork.java
@@ -63,9 +63,40 @@ import ecologylab.semantics.library.imdb.*;
 @xml_inherit
 
 public class  MmArtwork
-extends  Artwork
+extends  Metadata
 {
 
+
+/**
+	null
+**/ 
+
+	 @xml_nested private MetadataString	title;
+	@xml_collection("artists") private ArrayList<Author>	artists;
+
+/**
+	Abstract of the work.
+**/ 
+
+	@xml_tag("abstract") @xml_nested private MetadataString	abstractField;
+
+/**
+	Medium on which the work is done.
+**/ 
+
+	 @xml_nested private MetadataString	medium;
+
+/**
+	Year the work was created.
+**/ 
+
+	 @xml_nested private MetadataString	year;
+
+/**
+	Website of the work or group responsible.
+**/ 
+
+	 @xml_nested private MetadataParsedURL	website;
 
 /**
 	Link the ACM Portal page.
@@ -91,6 +122,328 @@ public MmArtwork(MetaMetadata metaMetadata)
 super(metaMetadata);
 }
 
+/**
+	Lazy Evaluation for title
+**/ 
+
+public MetadataString	title()
+{
+MetadataString	result	=this.title;
+if(result == null)
+{
+result = new MetadataString();
+this.title	=	 result;
+}
+return result;
+}
+
+/**
+	Gets the value of the field title
+**/ 
+
+public String getTitle(){
+return title().getValue();
+}
+
+/**
+	Sets the value of the field title
+**/ 
+
+public void setTitle( String title )
+{
+this.title().setValue(title);
+}
+
+/**
+	The heavy weight setter method for field title
+**/ 
+
+public void hwSetTitle( String title )
+{
+this.title().setValue(title);
+rebuildCompositeTermVector();
+ }
+/**
+	 Sets the title directly
+**/ 
+
+public void setTitleMetadata(MetadataString title)
+{	this.title = title;
+}
+/**
+	Heavy Weight Direct setter method for title
+**/ 
+
+public void hwSetTitleMetadata(MetadataString title)
+{	 if(this.title!=null && this.title.getValue()!=null && hasTermVector())
+		 termVector().remove(this.title.termVector());
+	 this.title = title;
+	rebuildCompositeTermVector();
+}
+/**
+	Lazy Evaluation for artists
+**/ 
+
+public  ArrayList<Author>	artists()
+{
+ ArrayList<Author>	result	=this.artists;
+if(result == null)
+{
+result = new  ArrayList<Author>();
+this.artists	=	 result;
+}
+return result;
+}
+
+/**
+	Set the value of field artists
+**/ 
+
+public void setArtists(  ArrayList<Author> artists )
+{
+this.artists = artists ;
+}
+
+/**
+	Get the value of field artists
+**/ 
+
+public  ArrayList<Author> getArtists(){
+return this.artists;
+}
+
+/**
+	Lazy Evaluation for abstractField
+**/ 
+
+public MetadataString	abstractField()
+{
+MetadataString	result	=this.abstractField;
+if(result == null)
+{
+result = new MetadataString();
+this.abstractField	=	 result;
+}
+return result;
+}
+
+/**
+	Gets the value of the field abstractField
+**/ 
+
+public String getAbstractField(){
+return abstractField().getValue();
+}
+
+/**
+	Sets the value of the field abstractField
+**/ 
+
+public void setAbstractField( String abstractField )
+{
+this.abstractField().setValue(abstractField);
+}
+
+/**
+	The heavy weight setter method for field abstractField
+**/ 
+
+public void hwSetAbstractField( String abstractField )
+{
+this.abstractField().setValue(abstractField);
+rebuildCompositeTermVector();
+ }
+/**
+	 Sets the abstractField directly
+**/ 
+
+public void setAbstractFieldMetadata(MetadataString abstractField)
+{	this.abstractField = abstractField;
+}
+/**
+	Heavy Weight Direct setter method for abstractField
+**/ 
+
+public void hwSetAbstractFieldMetadata(MetadataString abstractField)
+{	 if(this.abstractField!=null && this.abstractField.getValue()!=null && hasTermVector())
+		 termVector().remove(this.abstractField.termVector());
+	 this.abstractField = abstractField;
+	rebuildCompositeTermVector();
+}
+/**
+	Lazy Evaluation for medium
+**/ 
+
+public MetadataString	medium()
+{
+MetadataString	result	=this.medium;
+if(result == null)
+{
+result = new MetadataString();
+this.medium	=	 result;
+}
+return result;
+}
+
+/**
+	Gets the value of the field medium
+**/ 
+
+public String getMedium(){
+return medium().getValue();
+}
+
+/**
+	Sets the value of the field medium
+**/ 
+
+public void setMedium( String medium )
+{
+this.medium().setValue(medium);
+}
+
+/**
+	The heavy weight setter method for field medium
+**/ 
+
+public void hwSetMedium( String medium )
+{
+this.medium().setValue(medium);
+rebuildCompositeTermVector();
+ }
+/**
+	 Sets the medium directly
+**/ 
+
+public void setMediumMetadata(MetadataString medium)
+{	this.medium = medium;
+}
+/**
+	Heavy Weight Direct setter method for medium
+**/ 
+
+public void hwSetMediumMetadata(MetadataString medium)
+{	 if(this.medium!=null && this.medium.getValue()!=null && hasTermVector())
+		 termVector().remove(this.medium.termVector());
+	 this.medium = medium;
+	rebuildCompositeTermVector();
+}
+/**
+	Lazy Evaluation for year
+**/ 
+
+public MetadataString	year()
+{
+MetadataString	result	=this.year;
+if(result == null)
+{
+result = new MetadataString();
+this.year	=	 result;
+}
+return result;
+}
+
+/**
+	Gets the value of the field year
+**/ 
+
+public String getYear(){
+return year().getValue();
+}
+
+/**
+	Sets the value of the field year
+**/ 
+
+public void setYear( String year )
+{
+this.year().setValue(year);
+}
+
+/**
+	The heavy weight setter method for field year
+**/ 
+
+public void hwSetYear( String year )
+{
+this.year().setValue(year);
+rebuildCompositeTermVector();
+ }
+/**
+	 Sets the year directly
+**/ 
+
+public void setYearMetadata(MetadataString year)
+{	this.year = year;
+}
+/**
+	Heavy Weight Direct setter method for year
+**/ 
+
+public void hwSetYearMetadata(MetadataString year)
+{	 if(this.year!=null && this.year.getValue()!=null && hasTermVector())
+		 termVector().remove(this.year.termVector());
+	 this.year = year;
+	rebuildCompositeTermVector();
+}
+/**
+	Lazy Evaluation for website
+**/ 
+
+public MetadataParsedURL	website()
+{
+MetadataParsedURL	result	=this.website;
+if(result == null)
+{
+result = new MetadataParsedURL();
+this.website	=	 result;
+}
+return result;
+}
+
+/**
+	Gets the value of the field website
+**/ 
+
+public ParsedURL getWebsite(){
+return website().getValue();
+}
+
+/**
+	Sets the value of the field website
+**/ 
+
+public void setWebsite( ParsedURL website )
+{
+this.website().setValue(website);
+}
+
+/**
+	The heavy weight setter method for field website
+**/ 
+
+public void hwSetWebsite( ParsedURL website )
+{
+this.website().setValue(website);
+rebuildCompositeTermVector();
+ }
+/**
+	 Sets the website directly
+**/ 
+
+public void setWebsiteMetadata(MetadataParsedURL website)
+{	this.website = website;
+}
+/**
+	Heavy Weight Direct setter method for website
+**/ 
+
+public void hwSetWebsiteMetadata(MetadataParsedURL website)
+{	 if(this.website!=null && this.website.getValue()!=null && hasTermVector())
+		 termVector().remove(this.website.termVector());
+	 this.website = website;
+	rebuildCompositeTermVector();
+}
 /**
 	Lazy Evaluation for extendedAbstract
 **/ 
