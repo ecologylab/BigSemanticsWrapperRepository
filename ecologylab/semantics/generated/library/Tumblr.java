6022d13b99ad930d518ffa5dfcc3846a329393d9
andruid <andruid@ecologylab.net>
2010-06-03 02:32:12 +0000

    Metadata semantics & simpl tng integration. Metadata scalars as real scalar types (@xml_leaf, not @xml_nested). Bind meta-metadata with MetadataFieldDescriptor during MetaMetadataRepository.load. Resolve issues with translation scopes. Fix bug in initialization of ClassDescriptor subclasses.

--- ecologylab/semantics/generated/library/Tumblr.java
+++ ecologylab/semantics/generated/library/Tumblr.java
@@ -8,55 +8,28 @@ This is a generated code. DO NOT edit or modify it.
 
 
 
-import ecologylab.semantics.metadata.scalar.MetadataString;
-import ecologylab.semantics.metadata.scalar.MetadataInteger;
-import ecologylab.semantics.metadata.scalar.MetadataParsedURL;
-import ecologylab.semantics.metadata.DebugMetadata;
-import ecologylab.semantics.metadata.scalar.MetadataStringBuilder;
-import ecologylab.semantics.metadata.builtins.Document;
-import ecologylab.semantics.metadata.scalar.MetadataString;
-import ecologylab.semantics.metadata.scalar.MetadataInteger;
-import ecologylab.semantics.metadata.scalar.MetadataParsedURL;
-import ecologylab.semantics.metadata.builtins.Media;
-import ecologylab.semantics.metadata.DebugMetadata;
-import ecologylab.semantics.metadata.builtins.Image;
-import ecologylab.semantics.metadata.scalar.MetadataStringBuilder;
-import ecologylab.semantics.metadata.builtins.Entity;
-import ecologylab.semantics.metadata.Metadata;
-
- import java.util.*;
-import ecologylab.semantics.metametadata.MetaMetadata;
-import ecologylab.net.ParsedURL;
 import ecologylab.generic.HashMapArrayList;
-import ecologylab.xml.xml_inherit;
-import ecologylab.xml.types.element.Mappable;
-import ecologylab.semantics.metadata.DefaultMetadataTranslationSpace;
-import ecologylab.xml.TranslationScope;
-import ecologylab.xml.ElementState.xml_tag;
- import ecologylab.semantics.metadata.builtins.Document;
-import ecologylab.semantics.metadata.builtins.Media;
- import ecologylab.semantics.metadata.builtins.Image;
-import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.net.ParsedURL;
 import ecologylab.semantics.generated.library.*;
+import ecologylab.semantics.library.flickr.*;
+import ecologylab.semantics.library.imdb.*;
 import ecologylab.semantics.library.scholarlyPublication.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.flickr.*;
-import ecologylab.semantics.library.imdb.*;
-import ecologylab.semantics.library.imdb.*;
-import ecologylab.semantics.library.imdb.*;
-import ecologylab.semantics.library.imdb.*;
-import ecologylab.semantics.library.imdb.*;
 import ecologylab.semantics.library.uva.*;
-import ecologylab.semantics.library.scholarlyPublication.*;
-import ecologylab.semantics.library.scholarlyPublication.*;
-import ecologylab.semantics.library.scholarlyPublication.*;
-import ecologylab.semantics.generated.library.*;
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.MetadataBuiltinsTranslationScope;
+import ecologylab.semantics.metadata.builtins.*;
+import ecologylab.semantics.metadata.builtins.Document;
+import ecologylab.semantics.metadata.builtins.Entity;
+import ecologylab.semantics.metadata.builtins.Image;
+import ecologylab.semantics.metadata.builtins.Media;
+import ecologylab.semantics.metadata.scalar.*;
+import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.xml.ElementState.xml_tag;
+import ecologylab.xml.TranslationScope;
+import ecologylab.xml.types.element.Mappable;
+import ecologylab.xml.xml_inherit;
+import java.util.*;
 
 
 /**
--- ecologylab/semantics/library/flickr/FlickrAuthor.java
+++ ecologylab/semantics/library/flickr/FlickrAuthor.java
@@ -8,11 +8,28 @@ This is a generated code. DO NOT edit or modify it.
 
 
 
-import java.util.ArrayList;
-
+import ecologylab.generic.HashMapArrayList;
+import ecologylab.net.ParsedURL;
+import ecologylab.semantics.generated.library.*;
+import ecologylab.semantics.library.flickr.*;
+import ecologylab.semantics.library.imdb.*;
+import ecologylab.semantics.library.scholarlyPublication.*;
+import ecologylab.semantics.library.uva.*;
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.MetadataBuiltinsTranslationScope;
+import ecologylab.semantics.metadata.builtins.*;
 import ecologylab.semantics.metadata.builtins.Document;
+import ecologylab.semantics.metadata.builtins.Entity;
+import ecologylab.semantics.metadata.builtins.Image;
+import ecologylab.semantics.metadata.builtins.Media;
+import ecologylab.semantics.metadata.scalar.*;
 import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.xml.ElementState.xml_tag;
+import ecologylab.xml.TranslationScope;
+import ecologylab.xml.types.element.Mappable;
 import ecologylab.xml.xml_inherit;
+import java.util.*;
 
 
 /**
