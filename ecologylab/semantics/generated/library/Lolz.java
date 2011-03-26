3857a91327dc2ee9992f7353966bc2810fb51fe9
andruid <andruid@ecologylab.net>
2011-03-26 01:52:05 +0000

    just organize imports

--- ecologylab/semantics/generated/library/Lolz.java
+++ ecologylab/semantics/generated/library/Lolz.java
@@ -1,47 +1,12 @@
 package ecologylab.semantics.generated.library;
 
 
-import ecologylab.generic.HashMapArrayList;
 import ecologylab.net.ParsedURL;
-import ecologylab.semantics.generated.library.*;
-import ecologylab.semantics.generated.library.bibManaging.*;
-import ecologylab.semantics.generated.library.buzz.*;
-import ecologylab.semantics.generated.library.fastflip.*;
-import ecologylab.semantics.generated.library.flickr.*;
-import ecologylab.semantics.generated.library.googleBooks.*;
-import ecologylab.semantics.generated.library.icdl.*;
-import ecologylab.semantics.generated.library.imdb.*;
-import ecologylab.semantics.generated.library.misc.*;
-import ecologylab.semantics.generated.library.nsdl.*;
-import ecologylab.semantics.generated.library.opml.*;
-import ecologylab.semantics.generated.library.products.*;
-import ecologylab.semantics.generated.library.publication.*;
-import ecologylab.semantics.generated.library.rss.*;
-import ecologylab.semantics.generated.library.scholarlyPublication.*;
-import ecologylab.semantics.generated.library.scienceDirect.*;
-import ecologylab.semantics.generated.library.search.*;
-import ecologylab.semantics.generated.library.slashdot.*;
-import ecologylab.semantics.generated.library.urbanspoon.*;
-import ecologylab.semantics.generated.library.uva.*;
-import ecologylab.semantics.metadata.Metadata;
-import ecologylab.semantics.metadata.builtins.*;
-import ecologylab.semantics.metadata.builtins.ClippableMetadata;
-import ecologylab.semantics.metadata.builtins.Clipping;
-import ecologylab.semantics.metadata.builtins.DebugMetadata;
 import ecologylab.semantics.metadata.builtins.Document;
-import ecologylab.semantics.metadata.builtins.Entity;
-import ecologylab.semantics.metadata.builtins.Image;
-import ecologylab.semantics.metadata.builtins.Surrogate;
-import ecologylab.semantics.metadata.builtins.Text;
-import ecologylab.semantics.metadata.scalar.*;
+import ecologylab.semantics.metadata.scalar.MetadataParsedURL;
+import ecologylab.semantics.metadata.scalar.MetadataString;
 import ecologylab.semantics.metametadata.MetaMetadataCompositeField;
-import ecologylab.semantics.metametadata.MetaMetadataRepository;
-import ecologylab.serialization.ElementState.xml_tag;
-import ecologylab.serialization.Hint;
-import ecologylab.serialization.TranslationScope;
 import ecologylab.serialization.simpl_inherit;
-import ecologylab.serialization.types.element.Mappable;
-import java.util.*;
 
 
 /**
