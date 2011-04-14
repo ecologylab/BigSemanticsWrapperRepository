ea4222d3c6e7ddfb3e4fed390edcad4c0375bab4
andruid <andruid@ecologylab.net>
2011-04-14 13:33:21 +0000

    TNG -- NewMmTest works! HOORAY! Yahoo Buzz RSS (but not resulting searches) and remodelista wrapper work properly.
    informaworld extracts, but with standard HTMLDOMImageText, not with its own wrapper :-(

--- ecologylab/semantics/generated/library/GeneratedMetadataTranslationScope.java
+++ ecologylab/semantics/generated/library/GeneratedMetadataTranslationScope.java
@@ -7,9 +7,48 @@ This is a generated code. DO NOT edit or modify it.
 @author MetadataCompiler
 */
 
-import ecologylab.semantics.metadata.builtins.MetadataBuiltinsTranslationScope;
-import ecologylab.semantics.tools.MetaMetadataCompilerUtils;
+import ecologylab.generic.HashMapArrayList;
+import ecologylab.net.ParsedURL;
+import ecologylab.semantics.generated.library.*;
+import ecologylab.semantics.generated.library.bibManaging.*;
+import ecologylab.semantics.generated.library.buzz.*;
+import ecologylab.semantics.generated.library.fastflip.*;
+import ecologylab.semantics.generated.library.flickr.*;
+import ecologylab.semantics.generated.library.googleBooks.*;
+import ecologylab.semantics.generated.library.icdl.*;
+import ecologylab.semantics.generated.library.imdb.*;
+import ecologylab.semantics.generated.library.misc.*;
+import ecologylab.semantics.generated.library.nsdl.*;
+import ecologylab.semantics.generated.library.opml.*;
+import ecologylab.semantics.generated.library.products.*;
+import ecologylab.semantics.generated.library.publication.*;
+import ecologylab.semantics.generated.library.rss.*;
+import ecologylab.semantics.generated.library.scholarlyPublication.*;
+import ecologylab.semantics.generated.library.scienceDirect.*;
+import ecologylab.semantics.generated.library.search.*;
+import ecologylab.semantics.generated.library.slashdot.*;
+import ecologylab.semantics.generated.library.urbanspoon.*;
+import ecologylab.semantics.generated.library.uva.*;
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.builtins.*;
+import ecologylab.semantics.metadata.builtins.ClippableDocument;
+import ecologylab.semantics.metadata.builtins.Clipping;
+import ecologylab.semantics.metadata.builtins.DebugMetadata;
+import ecologylab.semantics.metadata.builtins.Document;
+import ecologylab.semantics.metadata.builtins.Entity;
+import ecologylab.semantics.metadata.builtins.Image;
+import ecologylab.semantics.metadata.builtins.ImageClipping;
+import ecologylab.semantics.metadata.builtins.MediaClipping;
+import ecologylab.semantics.metadata.builtins.TextClipping;
+import ecologylab.semantics.metadata.scalar.*;
+import ecologylab.semantics.metametadata.MetaMetadataCompositeField;
+import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.serialization.ElementState.xml_tag;
+import ecologylab.serialization.Hint;
 import ecologylab.serialization.TranslationScope;
+import ecologylab.serialization.simpl_inherit;
+import ecologylab.serialization.types.element.Mappable;
+import java.util.*;
 
 
 /**
@@ -246,6 +285,6 @@ public class GeneratedMetadataTranslationScope
  
 public static TranslationScope get()
 {
-return TranslationScope.get(MetaMetadataCompilerUtils.GENERATED_METADATA_TRANSLATIONS, MetadataBuiltinsTranslationScope.get(), TRANSLATIONS);
+return TranslationScope.get("generated_metadata_translations", MetadataBuiltinsTranslationScope.get(), TRANSLATIONS);
 }
 }
