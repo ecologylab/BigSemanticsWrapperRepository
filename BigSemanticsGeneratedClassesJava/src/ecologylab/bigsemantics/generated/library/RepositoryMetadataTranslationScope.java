41e0998cf8a189572e906fe42d55ab2c30785dde
amwebb <andrew@ecologylab.net>
2013-09-27 10:12:04 -0500

    add curation and information_composition_base meta-metadata. generate java classes for newly added meta-metadata.

--- BigSemanticsGeneratedClassesJava/src/ecologylab/bigsemantics/generated/library/RepositoryMetadataTranslationScope.java
+++ BigSemanticsGeneratedClassesJava/src/ecologylab/bigsemantics/generated/library/RepositoryMetadataTranslationScope.java
@@ -112,6 +112,10 @@ public class RepositoryMetadataTranslationScope
 
 		ecologylab.bigsemantics.generated.library.creative_work.CreativeWork.class,
 
+		ecologylab.bigsemantics.generated.library.creative_work.Curation.class,
+
+		ecologylab.bigsemantics.generated.library.creative_work.InformationCompositionBase.class,
+
 		ecologylab.bigsemantics.generated.library.creative_work.Periodical.class,
 
 		ecologylab.bigsemantics.generated.library.disaster.Disaster.class,
