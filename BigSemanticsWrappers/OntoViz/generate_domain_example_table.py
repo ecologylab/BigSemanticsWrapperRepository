8e13caad65b433562eac3fa72a9f7a0462d8b6fb
Yin Qu <me.yin.qu@gmail.com>
2014-02-20 01:28:55 -0600

    Make the script that generates domain-type-example table save results to a file when necessary.

--- BigSemanticsWrappers/OntoViz/generate_domain_example_table.py
+++ BigSemanticsWrappers/OntoViz/generate_domain_example_table.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import sys
 import json
 from urllib import urlencode
 # install package tldextract by:
@@ -98,7 +99,7 @@ def example_urls_by_domain_type(repo_json_file, is_filter):
     dfs(root, subtypes, lambda n: collect_example_urls(n, is_filter, results))
     return results
 
-def print_domain_example_json(results):
+def print_domain_example_json(results, f):
     objs = []
     for domain in sorted(results.keys()):
         obj = {}
@@ -108,15 +109,15 @@ def print_domain_example_json(results):
                                 for u in results[domain][t]])
         objs.append(obj)
     root = {'examples': objs}
-    print json.dumps(root)
+    json.dump(root, f)
 
-def print_domain_example_table(results):
-    print "<table>"
-    print "  <tr>"
-    print "    <th>Domain</th>"
-    print "    <th>Wrappers</th>"
-    print "    <th>Examples in MICE</th>"
-    print "  </tr>"
+def print_domain_example_table(results, f):
+    f.write("<table>\n")
+    f.write("  <tr>\n")
+    f.write("    <th>Domain</th>\n")
+    f.write("    <th>Wrappers</th>\n")
+    f.write("    <th>Examples in MICE</th>\n")
+    f.write("  </tr>\n")
     for d in sorted(results.keys()):
         s_types = [] 
         s_urls = []
@@ -127,12 +128,12 @@ def print_domain_example_table(results):
                 a = '<a href="' + mice_url + '" target="_blank">' +\
                     u + '</a>'
                 s_urls.append(a)
-        print "  <tr>" 
-        print "    <td>" + d + "</td>"
-        print "    <td>\n" + "<br/>\n".join(s_types) + "\n    </td>"
-        print "    <td>\n" + "<br/>\n".join(s_urls)+ "\n    </td>"
-        print "  </tr>" 
-    print "</table>"
+        f.write("  <tr>\n")
+        f.write("    <td>" + d + "</td>\n")
+        f.write("    <td>\n" + "<br/>\n".join(s_types) + "\n    </td>\n")
+        f.write("    <td>\n" + "<br/>\n".join(s_urls)+ "\n    </td>\n")
+        f.write("  </tr>\n") 
+    f.write("</table>\n")
 
 
 
@@ -145,12 +146,24 @@ if __name__ == '__main__':
     parser.add_argument('--type', type = str, default = 'json',
                         choices = ['json', 'html'],
                         help = 'Output type.')
+    parser.add_argument('--out', type = str, default = None,
+                        help = 'Output file.')
     parser.add_argument('--filter', type = bool, nargs = '?', const = True,
                         default = False, help = 'Filter domains for output.')
     args = parser.parse_args()
     results = example_urls_by_domain_type(args.repo, args.filter)
+    f = sys.stdout
+    if args.out is not None:
+        try:
+            f = open(args.out, 'wb')
+        except Exception as e:
+            sys.stderr.write("Cannot write output: " + e.message + "\n")
+            sys.exit(-1)
     if args.type == 'json':
-        print_domain_example_json(results)
+        print_domain_example_json(results, f)
     else:
-        print_domain_example_table(results)
+        print_domain_example_table(results, f)
+    f.flush()
+    if f is not sys.stdout:
+        f.close()
 
