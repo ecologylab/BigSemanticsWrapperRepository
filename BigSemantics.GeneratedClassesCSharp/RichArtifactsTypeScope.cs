c0b8799f6376483200cb2b167eeae9226658dd65
Andrew Webb <andrew@ecologylab.net>
2014-03-25 18:43:11 -0500

    add rich artifacts and rich media type scopes for c# and java to generated classes, so they can be used by any application.

---
+++ BigSemantics.GeneratedClassesCSharp/RichArtifactsTypeScope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ecologylab.Semantics.Generated.Library.PrimitivesNS;
+using Ecologylab.Semantics.MetadataNS.Builtins;
+using Simpl.Serialization;
+
+namespace Ecologylab.Semantics.Generated
+{
+    public class RichArtifactsTypeScope
+    {
+        public const string ScopeName = "rich_artifacts_scope";
+
+        protected static readonly Type[] Translations =
+            {
+                typeof (RichArtifact<>),
+                typeof (Clipping<>),
+                typeof (ImageClipping),
+                typeof (TextClipping),
+                typeof (VideoClipping),
+                typeof (ImageSelfmade),
+                typeof (TextSelfmade),
+                typeof (WebVideo),
+                typeof (HtmlText),
+                typeof (Image),
+                typeof (Video),
+                typeof (Audio)
+            };
+
+        public static SimplTypesScope Get()
+        {
+            return SimplTypesScope.Get(ScopeName, Translations);
+        }
+    }
+}
---
+++ BigSemantics.GeneratedClassesCSharp/RichMediaTypeScope.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Ecologylab.Semantics.Generated.Library.PrimitivesNS;
+using Ecologylab.Semantics.MetadataNS.Builtins;
+using Simpl.Serialization;
+
+namespace Ecologylab.Semantics.Generated
+{
+    public class RichMediaTypeScope
+    {
+        public static readonly string Name = "repository_media";
+
+        protected static readonly Type[] Translations =
+            {
+                typeof (HtmlText),
+                typeof (Image),
+                typeof (WebVideo),
+                typeof (Video),
+                typeof (Audio)
+            };
+
+        public static SimplTypesScope Get()
+        {
+            return SimplTypesScope.Get(Name, Translations);
+        }
+    }
+}
