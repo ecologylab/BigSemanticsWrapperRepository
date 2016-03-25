d2b7b2a9c89e92a2fd46143cb38f320fbfdc3cde
Ajit Jain <ajit.infotech@gmail.com>
2016-03-25 18:18:40 -0500

    use tweet as arg

--- BigSemanticsGeneratedClassesJava/src/ecologylab/bigsemantics/generated/library/search/TwitterSearchResults.java
+++ BigSemanticsGeneratedClassesJava/src/ecologylab/bigsemantics/generated/library/search/TwitterSearchResults.java
@@ -5,10 +5,10 @@ package ecologylab.bigsemantics.generated.library.search;
  *
  * DO NOT modify this code manually: All your changes may get lost!
  *
- * Copyright (2015) Interface Ecology Lab.
+ * Copyright (2016) Interface Ecology Lab.
  */
 
-import ecologylab.bigsemantics.generated.library.creativeWork.blogPost.Post;
+import ecologylab.bigsemantics.generated.library.creativeWork.blogPost.Tweet;
 import ecologylab.bigsemantics.generated.library.search.Search;
 import ecologylab.bigsemantics.metadata.builtins.MetadataBuiltinsTypesScope;
 import ecologylab.bigsemantics.metametadata.MetaMetadataCompositeField;
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @simpl_inherit
-public class TwitterSearchResults extends Search<Post>
+public class TwitterSearchResults extends Search<Tweet>
 {
 	public TwitterSearchResults()
 	{ super(); }
